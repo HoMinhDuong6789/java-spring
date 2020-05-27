30655dc09e93280939fde6627dca18cea9afc603
duongmh6789 <minhans29@gmail.com>
2020-05-27 22:56:34 +0700

    [4]lessonBookStore_backupDB

---
+++ database/Bookstore.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: localhost
+-- Thời gian đã tạo: Th5 27, 2020 lúc 05:54 PM
+-- Phiên bản máy phục vụ: 10.4.11-MariaDB
+-- Phiên bản PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `Bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `book`
+--
+
+CREATE TABLE `book` (
+  `book_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `author` varchar(45) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `book`
+--
+
+INSERT INTO `book` (`book_id`, `title`, `author`, `price`) VALUES
+(1, 'Ngay Xua Co Mot Chuyen Tinh', 'Nguyen Nhat Anh', 45000),
+(2, 'Ngay Xua Co Mot Chuyen Tinh 2', 'Nguyen Nhat Anh 2', 95000),
+(3, 'Ngay Xua Co Mot Chuyen Tinh 3', 'Nguyen Nhat Anh 3', 65000),
+(4, 'Ngay Xua Co Mot Chuyen Tinh 4', 'Nguyen Nhat Anh 4', 75000),
+(5, 'Ngay Xua Co Mot Chuyen Tinh 5', 'Nguyen Nhat Anh 5', 65000);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`),
+  ADD UNIQUE KEY `book_id_UNIQUE` (`book_id`),
+  ADD UNIQUE KEY `title_UNIQUE` (`title`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `book`
+--
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
