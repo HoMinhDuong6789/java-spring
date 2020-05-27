00c0157dca118139bf2e049cac85f08519ecd15d
duongmh6789 <minhans29@gmail.com>
2020-05-27 21:26:15 +0700

    lessonBookStore_create_model_dao_layer

--- BookStore/src/dao/BookDAO.java
+++ BookStore/src/dao/BookDAO.java
@@ -25,23 +25,6 @@ public class BookDAO {
 		this.jdbcConnection = jdbcConnection;
 	}
 	
-	
-	/*
-	 * protected static void connect () throws SQLException{ try{
-	 * Class.forName("com.mysql.jdbc.Driver"); Connection
-	 * con=DriverManager.getConnection(
-	 * "jdbc:mysql://localhost:3308/Bookstore","root",""); //here sonoo is database
-	 * name, root is username and password Statement stmt=con.createStatement();
-	 * ResultSet rs=stmt.executeQuery("select * from book"); while(rs.next())
-	 * System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)
-	 * +"  "+rs.getString(4)); con.close(); }catch(Exception e){
-	 * System.out.println(e);} }
-	 */
-	
-	public static void main(String[] args) {
-		BookDAO bk = new BookDAO("", "", "", "");
-	}
-	
 	protected void connect() throws SQLException {
         if (jdbcConnection == null || jdbcConnection.isClosed()) {
             try {
