f840ed7c7cbe14131566ed38d46ef92021ae684b
minh6789 <minhans29@gmail.com>
2020-06-08 17:05:10 +0700

    [2] (jpa-spring-mvc) jpa, Spring Data JPA in spring mvc and mysql part 5

--- spring-mvc/src/main/java/com/laptrinhjavaweb/dao/impl/AbstractDAO.java
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/dao/impl/AbstractDAO.java
@@ -17,13 +17,20 @@ import com.laptrinhjavaweb.mapper.RowMapper;
 public class AbstractDAO<T> implements GenericDAO<T> {
 
 	ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
-	
+
 	public Connection getConnection() {
 		try {
-			Class.forName(resourceBundle.getString("driverName"));
-			String url = resourceBundle.getString("url");
-			String user = resourceBundle.getString("user");
-			String password = resourceBundle.getString("password");
+			/*
+			 * Class.forName(resourceBundle.getString("driverName")); String url =
+			 * resourceBundle.getString("url"); String user =
+			 * resourceBundle.getString("user"); String password =
+			 * resourceBundle.getString("password");
+			 */
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/jspservletjdbc";
+			String user = "root";
+			String password ="";
+
 			return DriverManager.getConnection(url, user, password);
 		} catch (ClassNotFoundException | SQLException e) {
 			return null;
