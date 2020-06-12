f26077646aa52e4bcac948ded47809a9df9f9ca1
minh6789 <minhans29@gmail.com>
2020-06-12 14:13:16 +0700

    [0] (levunguyen-java) Init project

---
+++ security-web/src/main/java/com/example/securityweb/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.securityweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurer{
+	public void AddViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+		
+	}
+}
