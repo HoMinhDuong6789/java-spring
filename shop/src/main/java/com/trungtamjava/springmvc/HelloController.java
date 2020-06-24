47fe299c4c7e95b2e9d182d3792d0d252ad60879
duongmh6789 <minhans29@gmail.com>
2020-06-24 09:58:37 +0700

    [] (bai07) RequestParam

--- shop/src/main/java/com/trungtamjava/springmvc/HelloController.java
+++ shop/src/main/java/com/trungtamjava/springmvc/HelloController.java
@@ -5,15 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping("/say-hello")
-	public ModelAndView sayHello(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
-		request.setAttribute("msg", "J4team");
+	public ModelAndView sayHello(HttpServletRequest request, HttpServletResponse response, HttpSession session,
+			@RequestParam(name = "user", required = true) String username,
+			@RequestHeader(name = "Accept", required = false) String contentType) {
+		request.setAttribute("msg", username+" "+contentType);
 		return new ModelAndView("hello");
 	}
 }
