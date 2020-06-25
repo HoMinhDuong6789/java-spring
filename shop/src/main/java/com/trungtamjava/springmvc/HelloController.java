ec4c86933772301006f2da233e2d6fa0a28d3cb1
duongmh6789 <minhans29@gmail.com>
2020-06-25 13:56:26 +0700

    [] (Bai13)  Spring Download file

--- shop/src/main/java/com/trungtamjava/springmvc/HelloController.java
+++ shop/src/main/java/com/trungtamjava/springmvc/HelloController.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -149,5 +152,23 @@ public class HelloController {
 		}
 		request.setAttribute("files", files);
 		return "viewFile";
+	}
+
+	@RequestMapping(value = "/download-file", method = RequestMethod.GET)
+	public void download(HttpServletRequest request, HttpServletResponse response) {
+		String dataDirectory = "/home/minhpc/workspaces/java-spring/shop/file/";
+		Path file = Paths.get(dataDirectory, "aa.jpeg");
+		if (Files.exists(file)) {
+			response.setContentType("image/jpeg");
+			response.addHeader("Content-Disposition", "attachment; picture.jpeg");
+		}
+
+		try {
+			Files.copy(file, response.getOutputStream());
+			response.getOutputStream().flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
