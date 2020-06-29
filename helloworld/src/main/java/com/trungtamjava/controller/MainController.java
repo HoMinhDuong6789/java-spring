a10eafe5428415ab3f3f2ffed8f9709fcf1413f1
duongmh6789 <minhans29@gmail.com>
2020-06-29 22:00:37 +0700

    [3] (Bai2) Source Message.properties Configuaration

--- helloworld/src/main/java/com/trungtamjava/controller/MainController.java
+++ helloworld/src/main/java/com/trungtamjava/controller/MainController.java
@@ -2,14 +2,21 @@ package com.trungtamjava.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	MessageSource messageSource;
+	
 	@GetMapping(value = "/")
 	public String home(HttpServletRequest request) {
-		request.setAttribute("msg", "Hello Spring Boot");
+		//request.setAttribute("msg", "Hello Spring Boot");
+		request.setAttribute("msg", messageSource.getMessage("my.name", null, null));
 		return "index";
 	}
 }
--- helloworld/src/main/java/com/trungtamjava/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/trungtamjava/helloworld/HelloworldApplication.java
@@ -2,6 +2,8 @@ package com.trungtamjava.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = {"com.trungtamjava"})
 public class HelloworldApplication {
@@ -9,5 +11,7 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
+	
+	
 
 }
