e3e39823e12323006b54c99b9725406bf9cbfa1b
duongmh6789 <minhans29@gmail.com>
2020-07-18 14:04:11 +0700

    [1] (Bai8) Upload file

---
+++ helloworld/src/main/java/com/trungtamjava/controller/FileController.java
@@ -0,0 +1,26 @@
+package com.trungtamjava.controller;
+
+import java.io.File;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileController {
+	
+	
+	@GetMapping("/upload-file")
+	public String upload() {
+		return "uploadFile";
+	}
+	
+
+	@PostMapping("/upload-file")
+	public String upload(@RequestParam(name = "file") MultipartFile file) {
+		System.out.println("############file:"+file.getOriginalFilename());
+		return "uploadFile";
+	}
+}
