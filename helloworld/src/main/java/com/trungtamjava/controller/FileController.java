427e13a6a07a0c99a3e22ee3eb67c61ed4f4d76a
duongmh6789 <minhans29@gmail.com>
2020-07-18 14:13:06 +0700

    [3] (Bai08) Upload file - Configuation Size of file upload

--- helloworld/src/main/java/com/trungtamjava/controller/FileController.java
+++ helloworld/src/main/java/com/trungtamjava/controller/FileController.java
@@ -1,7 +1,5 @@
 package com.trungtamjava.controller;
 
-import java.io.File;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
