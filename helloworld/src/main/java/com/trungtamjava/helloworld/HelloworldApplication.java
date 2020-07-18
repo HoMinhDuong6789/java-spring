969dd61e4bfc41b4e0209bb302d57b31287f681c
duongmh6789 <minhans29@gmail.com>
2020-07-18 16:57:25 +0700

    [0] (Bai10) Spring boot JPA Hibernate Configuaration

--- helloworld/src/main/java/com/trungtamjava/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/trungtamjava/helloworld/HelloworldApplication.java
@@ -2,10 +2,12 @@ package com.trungtamjava.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = {"com.trungtamjava"})
+@EntityScan(basePackages = {"com.trungtamjava.entity"})
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
