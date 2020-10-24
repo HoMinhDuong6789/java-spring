2f478c7e9034a314fee569d3ca3387ae10d53ba5
Duong Ho <duong.ho@infodation.vn>
2020-10-24 22:09:17 +0700

    9. Generic Method

--- GenericClassLesson/src/main/java/com/hmd/App.java
+++ GenericClassLesson/src/main/java/com/hmd/App.java
@@ -4,19 +4,11 @@ package com.hmd;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       var countries = new String[] {"Vietnam", "Thailand", "Compudia"};
-       var names = new String[] {"John", "James", "Daniel"};
-       var scores = new Integer[] {22, 46, 66,54};
-       
-       var store1 = new Store<>(countries);
-       var store2 = new Store<>(names);
-       var store3 = new Store<>(scores);
-       
-       store1.greaterThan(store2);
-       
-    }
+public class App {
+	public static void main(String[] args) {
+		var store = new Store();
+
+		store.<Integer>print(90);
+
+	}
 }
--- GenericClassLesson/src/main/java/com/hmd/Store.java
+++ GenericClassLesson/src/main/java/com/hmd/Store.java
@@ -1,19 +1,13 @@
 package com.hmd;
 
-public class Store<U> {
-	U[] val1;
-
-	public Store(U[] val1) {
-		this.val1 = val1;
-
+public class Store {
+	
+	public void open () {
+		System.out.println("Open.........");
 	}
-
-	public void greaterThan(Store<U> otherStore) {
-		//public void greaterThan(Store<?> otherStore) {
-		if (val1.length > otherStore.val1.length) {
-			System.out.println("true");
-		} else {
-			System.out.println("false");
-		}
+	
+	public <T>void print (T num) {
+		 System.out.println(num);
 	}
+	
 }
